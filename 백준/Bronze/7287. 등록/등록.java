4f1f025aad3148f64fd71734e5cdf62092bae0a1
NaSangJoon <107744931+Nasangjoon@users.noreply.github.com>
2026-02-10 19:11:09 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 첫째 줄: 맞힌 문제 수
+        System.out.println(81);
+        
+        // 둘째 줄: 아이디
+        System.out.println("sangjoon1002");
+    }
+}
