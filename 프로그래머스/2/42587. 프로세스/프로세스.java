21e9072562c553379ff920eb65832e432e23c1e5
NaSangJoon <107744931+Nasangjoon@users.noreply.github.com>
2026-06-23 09:38:40 +0900

    [level 2] Title: 프로세스, Time: 2.10 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,36 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < priorities.length; i++) {
+            queue.add(new int[]{i, priorities[i]});
+        }
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            boolean hasHigher = false;
+
+            for (int[] p : queue) {
+                if (p[1] > current[1]) {
+                    hasHigher = true;
+                    break;
+                }
+            }
+
+            if (hasHigher) {
+                queue.add(current);
+            } else {
+                answer++;
+                if (current[0] == location) {
+                    return answer;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
